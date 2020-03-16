e975de7d2c3e89194de3799a1f7229440da69b68
Hamilton Evans <hevans@middlebury.edu>
2020-03-16 16:11:04 -0700

    team class created

--- SoccerNetwork.py
+++ SoccerNetwork.py
@@ -1,18 +1,33 @@
+import matplotlib.pyplot as plt
+import matplotlib.animation as animation
+
 class Player:
     xLoc = 0
     yLoc = 0
     angle = 0
     speed = 0
     accelData = [0,0,0]
+    momentum =0
     
-    def __init__(self, team=0):
+    def __init__(self, position, team):
         self.team = team
+        self.position = position
     
-    def add(self, val):
+    def addAccel(self, val):
          self.accelData=self.accelData[1:]+[val]
+         self.momentum = self.accelData[-1] - self.accelData[0]
           
-#    def momentum (self):
         
+class Team:     
+    def __init__ (self, team):
+        self.team = team
+        self.players = []
+        for i in range (1,12): 
+            self.players.append(Player(i,self.team))
+      
+            
+test = Team('woooo')
+players = test.players
 
-test = Player()
-
+players[0].addAccel(5)
+print(players[0].momentum)
