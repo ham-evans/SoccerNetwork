5abd30408702d49da146b5209e7fd66f1a7f3864
Hamilton Evans <hevans@middlebury.edu>
2020-03-16 15:50:52 -0700

    Player class instantiated

--- SoccerNetwork.py
+++ SoccerNetwork.py
@@ -1,8 +1,18 @@
-#!/usr/bin/env python3
-# -*- coding: utf-8 -*-
-"""
-Created on Mon Mar 16 10:20:11 2020
+class Player:
+    xLoc = 0
+    yLoc = 0
+    angle = 0
+    speed = 0
+    accelData = [0,0,0]
+    
+    def __init__(self, team=0):
+        self.team = team
+    
+    def add(self, val):
+         self.accelData=self.accelData[1:]+[val]
+          
+#    def momentum (self):
+        
 
-@author: hamevans
-"""
+test = Player()
 
